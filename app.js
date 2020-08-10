2caaa9ceb29623dda03dd950630c822edd47272e
JustinLaw-dev <justinlaw31@gmail.com>
2020-08-10 15:20:22 -0700

    updated tabs
    changed logo class to player, added styling for selected class, and changed logic to use JS for tab switching

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //Tab functionality for header
 const players = document.querySelectorAll('.player');
 
-function clickHandler() {
-  console.log('Hello World');
+function clickHandler(event) {
+  players.forEach((player) => {
+    player.classList.remove('selected');
+  });
+  event.currentTarget.classList.add('selected');
 }
+// console.log(event.currentTarget);
 
 players.forEach((player) => {
   player.addEventListener('click', clickHandler);
