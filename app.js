4f7884a0492b757f318d105c57b0dd73108759c9
Justin Law <justinlaw31@gmail.com>
2020-08-16 15:20:34 -0700

    added navbar, player image logic
    created navbar with social media.
    added initial player hover outline and scale

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-//Tab functionality for header
-const players = document.querySelectorAll('.player');
+// //Tab functionality for header
+// const players = document.querySelectorAll('.player');
 
-function clickHandler(event) {
-  players.forEach((player) => {
-    player.classList.remove('selected');
-  });
-  event.currentTarget.classList.add('selected');
-}
-// console.log(event.currentTarget);
+// function clickHandler(event) {
+//   players.forEach((player) => {
+//     player.classList.remove('selected');
+//   });
+//   event.currentTarget.classList.add('selected');
+// }
+// // console.log(event.currentTarget);
 
-players.forEach((player) => {
-  player.addEventListener('click', clickHandler);
-});
+// players.forEach((player) => {
+//   player.addEventListener('click', clickHandler);
+// });
+const player = document.querySelector('.landing__composition--player');
+
+player.onmouseover = function () {
+  player.setAttribute('src', './img/kuz outline.png');
+};
+
+player.onmouseout = function () {
+  player.setAttribute('src', './img/kuz.png');
+};
