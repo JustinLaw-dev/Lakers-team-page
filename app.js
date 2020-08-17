a80caf327a811c9a119fd837a10bf844736a1ec5
Justin Law <justinlaw31@gmail.com>
2020-08-16 17:37:33 -0700

    updated app.js
    created new logic for outline on hover, using event listeners

--- app.js
+++ app.js
@@ -16,11 +16,11 @@
 //TODO: make player img ID the same as link to image. Then use back
 //ticks to create a string with ${this.id} so you can use one logic
 // to apply to all
+//done
 const players = document.querySelectorAll('.landing__composition--player');
 
 function enterHandler() {
   this.setAttribute('src', `./img/${this.id} outline.png`);
-  // player.setAttribute('src', './img/kuz outline.png');
 }
 
 function leaveHandler() {
@@ -31,7 +31,3 @@ players.forEach((player) => {
   player.addEventListener('mouseenter', enterHandler);
   player.addEventListener('mouseleave', leaveHandler);
 });
-
-// player.onmouseout = function () {
-//   player.setAttribute('src', './img/kuz.png');
-// };
