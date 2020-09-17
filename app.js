7f65ee2615aea6e9feda1eefda0e835f9004bf15
Justin Law <justinlaw31@gmail.com>
2020-09-16 21:01:39 -0700

    table in progress
    implement table on click
    
    ~TODO~
    -  multiple clicks is multiple tables
    - adjust table sizing

--- app.js
+++ app.js
@@ -15,7 +15,63 @@ function leaveHandler() {
 function showStats(e) {
   players.forEach((player) => {
     if (e.currentTarget.alt != player.alt) {
-      player.style.opacity = 0;
+      // player.style.opacity = 0;
+      player.style.display = 'none';
+    }
+    if (e.currentTarget.alt === player.alt) {
+      console.log('1');
+      d3.text('AB_data.csv').then(function (datasetText) {
+        var rows = d3.csvParseRows(datasetText),
+          table = d3
+            .select('#tableContainer')
+            .append('table')
+            .style('border-collapse', 'collapse')
+            .style('border', '2px black solid');
+
+        // headers
+        table
+          .append('thead')
+          .append('tr')
+          .selectAll('th')
+          .data(rows[0])
+          .enter()
+          .append('th')
+          .text(function (d) {
+            return d;
+          })
+          .style('border', '1px black solid')
+          .style('padding', '5px')
+          .style('background-color', 'lightgray')
+          .style('font-weight', 'bold')
+          .style('text-transform', 'uppercase');
+
+        // data
+        table
+          .append('tbody')
+          .selectAll('tr')
+          .data(rows.slice(1))
+          .enter()
+          .append('tr')
+          .selectAll('td')
+          .data(function (d) {
+            return d;
+          })
+          .enter()
+          .append('td')
+          .style('border', '1px black solid')
+          .style('padding', '5px')
+          .style('background-color', 'white')
+          // .on('mouseover', function () {
+          //   d3.select(this).style('background-color', 'powderblue');
+          // })
+          // .on('mouseout', function () {
+          //   d3.select(this).style('background-color', 'white');
+          // })
+          .text(function (d) {
+            return d;
+          })
+          .style('font-size', '12px');
+      });
     }
   });
 }
